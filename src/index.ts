6fe82674e5390b06e76e3be51017e73267465880
Archakov Dennis <hello@archakov.im>
2020-12-02 17:44:33 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,46 @@
 interface Options {
   root: string;
   state?: any;
+  app: ElementProps;
 }
 
-class SPA {
+interface ElementProps {
+  tagName: string;
+  props?: Record<string, any>;
+  children: ElementProps | string | Array<ElementProps | string>;
+}
+
+class SPA<TState extends Record<string, unknown>> {
   public root: HTMLElement | null;
-  public state: any;
-  public cacheApp: HTMLElement | null;
+  public state: TState;
+  private app: ElementProps;
 
   constructor(options: Options) {
     this.root = document.querySelector(options.root);
-    this.state = options.state;
-    this.cacheApp = null;
+
     if (!this.root) {
       throw Error('Root element not found');
     }
+
+    this.state = options.state;
+    this.app = options.app;
+
+    this.render();
   }
 
   createElement(
-    tagName: string,
-    props: any,
-    children: HTMLElement | string | Array<HTMLElement | string>,
-  ) {
+    {
+      tagName,
+      props,
+      children
+    }: ElementProps
+  ): HTMLElement {
     if (!this.root) {
       throw Error('Root element not found');
     }
-
+  
     const elem = document.createElement(tagName);
-
+  
     if (props) {
       for (const key in props) {
         const value = props[key];
@@ -38,8 +51,8 @@ class SPA {
         }
       }
     }
-
-    const appendChild = (value: HTMLElement | string) => {
+  
+    const appendChild = (value: HTMLElement | string): void => {
       if (value instanceof HTMLElement) {
         elem.appendChild(value);
       }
@@ -48,71 +61,83 @@ class SPA {
         text = text.replace(/{(.+?)}/gi, match => {
           const key = match.replace(/\{|\}|\s/g, '');
           return this.state && Object.prototype.hasOwnProperty.call(this.state, key)
-            ? this.state[key]
+            ? this.state[key] as string
             : match;
         });
         elem.innerText = text;
       }
     };
 
+    const appendStringOrElement = (child: string | ElementProps): void => {
+      if (typeof child === 'string') {
+        appendChild(child);
+      } else {
+        appendChild(this.createElement(child));
+      }
+    };
+  
     if (children) {
       if (Array.isArray(children)) {
         for (let i = 0; i < children.length; i++) {
-          appendChild(children[i]);
+          const child = children[i];
+          appendStringOrElement(child);
         }
       } else {
-        appendChild(children);
+        appendStringOrElement(children);
       }
     }
-
+  
     return elem;
   }
 
-  update(newState: any) {
+  update(callback: (newState: TState) => TState): void {
     if (
       this.state &&
       this.state instanceof Object &&
-      newState &&
-      newState instanceof Object &&
-      this.cacheApp
+      callback &&
+      callback instanceof Function &&
+      this.root
     ) {
+      const newState = callback(this.state);
       this.state = Object.assign(this.state, newState);
-      this.render(this.cacheApp);
+      this.render();
     }
   }
 
-  render(app: HTMLElement) {
+  render(): void {
     if (this.root) {
+      const htmlElem = this.createElement(this.app);
       this.root.innerHTML = '';
-      this.root.appendChild(app);
-      this.cacheApp = app;
+      this.root.appendChild(htmlElem);
     } else {
       throw Error('Root element not found');
     }
   }
 }
 
-const app = new SPA({
-  root: '#app',
-  state: {
-    name: 'World!',
-  },
-});
-
-app.render(
-  app.createElement('div', { style: 'color: red', class: 'app' }, [
-    app.createElement('p', null, 'Hello, {name}'),
-    app.createElement(
-      'button',
-      {
+const app: ElementProps = {
+  tagName: 'div',
+  children: [
+    { tagName: 'h2', children: 'Счетчик'},
+    { tagName: 'h2', children: '{count}'},
+    {
+      tagName: 'button',
+      props: {
         click: () => {
-          app.update({
-            name: 'Archakov!',
-          });
-          alert('Updated');
+          spa.update(prev => ({
+            count: prev.count + 1,
+          }));
         },
       },
-      'Update name',
-    ),
-  ]),
-);
+      children: 'Increment'
+    },
+  ]
+};
+
+const spa = new SPA<{ count: number }>({
+  root: '#app',
+  state: {
+    count: 0,
+  },
+  app: app
+});
